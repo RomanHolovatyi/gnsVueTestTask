336841c1f112bc0cb4861155ae116d5b4e5be85f
RomanHolovatyi <roma.holovatyi@gmail.com>
2018-09-02 13:59:13 +0300

    Add empty components

---
+++ src/components/TableItemDetails/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+
+  export default {
+    name: 'TableItemDetails'
+  }
+</script>
+
+<style scoped>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Table from '@/components/Table'
+import TableItemDetails from '@/components/TableItemDetails'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Table',
+      component: Table
+    },
+    {
+      path: '/table-item',
+      name: 'TableItemDetails',
+      component: TableItemDetails
+    }
+  ]
+})
